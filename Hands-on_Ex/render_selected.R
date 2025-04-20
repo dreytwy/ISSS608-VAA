30d172a0778d4d3c49c17ba03d3c161f975e22f2
dreytwy <drey.wenying@gmail.com>
2025-04-20 22:14:13 +0800

    Commit

---
+++ Hands-on_Ex/render_selected.R
@@ -0,0 +1,3 @@
+quarto::quarto_render("Hands-on_Ex/Hands-on_Ex01/Hands-on_Ex01.qmd")
+quarto::quarto_render("Hands-on_Ex/Hands-on_Ex02/Hands-on_Ex02.qmd")
+quarto::quarto_render("Hands-on_Ex/Hands-on_Ex03A/Hands-on_Ex03A.qmd")
