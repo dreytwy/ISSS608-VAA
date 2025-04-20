2f1d508a5a455c28dc62fe20fe3b45e241f9d15a
dreytwy <drey.wenying@gmail.com>
2025-04-21 00:56:04 +0800

    commit after testing again

--- Hands-on_Ex/render_selected.R
+++
@@ -1,3 +0,0 @@
-quarto::quarto_render("Hands-on_Ex/Hands-on_Ex01/Hands-on_Ex01.qmd")
-quarto::quarto_render("Hands-on_Ex/Hands-on_Ex02/Hands-on_Ex02.qmd")
-quarto::quarto_render("Hands-on_Ex/Hands-on_Ex03A/Hands-on_Ex03A.qmd")
